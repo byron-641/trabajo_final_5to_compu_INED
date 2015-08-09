303ca50760754dd2ee18fd5b437dbdf953085462
byron-641 <byron6415@gmail.com>
2015-08-08 21:24:03 -0600

    Proyecto al 10%
    Agregar la base de datos

---
+++ trabajo final reloaded/trabajo final reloaded/coneccion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+namespace trabajo_final_reloaded
+{
+    class coneccion
+    {
+        public static string con = @"C:\Users\GGM7\Documents\GitHub\trabajo_final_5to_compu_INED\trabajo final reloaded\trabajo final reloaded\proyecto.accdb";
+        
+    }
+}
---
+++ trabajo final reloaded/trabajo final reloaded/creadordatos.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace trabajo_final_reloaded
+{
+    public partial class creadordatos : Form
+    {
+        public creadordatos()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+                {
+                    OleDbConnection miconeccion = new OleDbConnection(coneccion.con);
+                    OleDbCommand cguardar = new OleDbCommand("insert into planillas(Id,clave,nombre1,nombre2,apellido1,apellido2,grado,orientacion)values(@Id,@clave,@nombre1,@nombre2,@apellido1,@apellido2,@grado,@orientacion)", miconeccion) ;
+                    cguardar.Parameters.AddWithValue("nombre1",textBox1.Text);
+                    cguardar.Parameters.AddWithValue("nombre2", textBox2.Text);
+                    cguardar.Parameters.AddWithValue("apellido1", textBox3.Text);
+                    cguardar.Parameters.AddWithValue("apellido2", textBox4.Text);
+                    cguardar.Parameters.AddWithValue("Id", textBox5.Text);
+                    cguardar.Parameters.AddWithValue("orientacion", textBox6.Text);
+                    miconeccion.Open();
+                    cguardar.ExecuteNonQuery();
+                    administrador administrador = new administrador();
+                    administrador.Show();
+                    MessageBox.Show("Datos Guardados Exitosamente","Informacion del Sistema",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                    miconeccion.Close();
+                    this.Hide();
+                
+                }
+            catch (Exception)
+                 {
+                     MessageBox.Show("mula");
+                
+                 }
+            
+        }
+    }
+}
